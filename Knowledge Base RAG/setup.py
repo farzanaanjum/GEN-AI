ebef56684a74f03dd8ba523b3d1fb28037548a12
Farzana Anjum <30755050+farzanaanjum@users.noreply.github.com>
2025-01-10 22:49:46 +0530

    Add files via upload

---
+++ Knowledge Base RAG/setup.py
@@ -0,0 +1,62 @@
+import json
+import os
+import boto3
+from botocore.exceptions import ClientError
+import pprint
+from utility import create_bedrock_execution_role, create_oss_policy_attach_bedrock_execution_role, create_policies_in_oss, interactive_sleep
+import random
+from retrying import retry
+
+suffix = random.randrange(200, 900)
+
+sts_client = boto3.client('sts')
+boto3_session = boto3.session.Session()
+region_name = boto3_session.region_name
+bedrock_agent_client = boto3_session.client('bedrock-agent', region_name=region_name)
+service = 'aoss'
+s3_client = boto3.client('s3')
+account_id = sts_client.get_caller_identity()["Account"]
+s3_suffix = f"{region_name}-{account_id}"
+bucket_name = f'bedrock-kb-{s3_suffix}' # replace it with your bucket name.
+pp = pprint.PrettyPrinter(indent=2)
+
+# Check if bucket exists, and if not create S3 bucket for knowledge base data source
+try:
+    s3_client.head_bucket(Bucket=bucket_name)
+    print(f'Bucket {bucket_name} Exists')
+except ClientError as e:
+    print(f'Creating bucket {bucket_name}')
+    if region_name == "us-east-1":
+        s3bucket = s3_client.create_bucket(
+            Bucket=bucket_name)
+    else:
+        s3bucket = s3_client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={ 'LocationConstraint': region_name }
+    )
+
+# Ctreate Vector
+import boto3
+import time
+vector_store_name = f'bedrock-sample-rag-{suffix}'
+index_name = f"bedrock-sample-rag-index-{suffix}"
+aoss_client = boto3_session.client('opensearchserverless')
+bedrock_kb_execution_role = create_bedrock_execution_role(bucket_name=bucket_name)
+bedrock_kb_execution_role_arn = bedrock_kb_execution_role['Role']['Arn']
+
+# create security, network and data access policies within OSS
+encryption_policy, network_policy, access_policy = create_policies_in_oss(vector_store_name=vector_store_name,
+                       aoss_client=aoss_client,
+                       bedrock_kb_execution_role_arn=bedrock_kb_execution_role_arn)
+collection = aoss_client.create_collection(name=vector_store_name,type='VECTORSEARCH')
+
+pp.pprint(collection)
+
+# Get the OpenSearch serverless collection URL
+collection_id = collection['createCollectionDetail']['id']
+host = collection_id + '.' + region_name + '.aoss.amazonaws.com'
+print(host)
+
+
+
+
